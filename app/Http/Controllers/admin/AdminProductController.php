7091a8b36ef43c2dd3d7c4485c19aaad31bf7c95
dokhala <dokhaclam@gmail.com>
2021-10-15 23:47:34 +0700

    debug image gallery 1

--- app/Http/Controllers/admin/AdminProductController.php
+++ app/Http/Controllers/admin/AdminProductController.php
@@ -194,6 +194,8 @@ class AdminProductController extends Controller
         
         //check features thong qua category relation co with features
         $check_features = $product -> category() -> get();
+
+        ProductGallery::where('id',150) -> delete();
       
         //get image gallery
         $image_gallery = $product -> gallery() -> get();
